8c5cc2e3de5665509120a9d2a53e629e6764a0cd
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 10:07:46 +0530

    Add files via upload

---
+++ LINQ Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+
+namespace LINQ_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intNumbers = new int[] { 10, 30, 50, 40, 60, 20, 70, 90, 80, 100 };
+            //Using Method Syntax
+            int MSTotal = intNumbers.Sum();
+            //Using Query Syntax
+            int QSTotal = (from num in intNumbers
+                           select num).Sum();
+            Console.WriteLine("Sum = " + QSTotal);
+            Console.ReadKey();
+        }
+    }
+}
