d17169a3b483a30bb2d3d524478870b5d3f1a1c3
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 10:21:08 +0530

    Add files via upload

---
+++ LINQ Count/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+   
+
+namespace LINQ_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intNumbers = new int[] { 60, 80, 50, 90, 10, 30, 70, 40, 20, 100 };
+            //Using Method Syntax
+            int MSCount = intNumbers.Count();
+            //Using Query Syntax
+            var QSCount = (from num in intNumbers
+                           select num).Count();
+            Console.WriteLine("No of Elements = " + MSCount);
+            Console.ReadKey();
+        }
+    }
+}
