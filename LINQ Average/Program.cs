5b05901ac47c4d4887119ab62ae6327dbd92502b
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 10:18:22 +0530

    Add files via upload

---
+++ LINQ Average/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intNumbers = new int[] { 60, 80, 50, 90, 10, 30, 70, 40, 20, 100 };
+            //Using Method Syntax
+            var MSAverageValue = intNumbers.Average();
+            //Using Query Syntax
+            var QSAverageValue = (from num in intNumbers
+                                  select num).Average();
+            Console.WriteLine("Average Value = " + MSAverageValue);
+            Console.ReadKey();
+        }
+    }
+}
