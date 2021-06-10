36cac2140c7fb6cb32a684dcd2c791fe9f90f5c2
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 10:10:07 +0530

    Add files via upload

---
+++ LINQ Max/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intNumbers = new int[] { 10, 80, 50, 90, 60, 30, 70, 40, 20, 100 };
+            //Using Method Syntax
+            int MSLergestNumber = intNumbers.Max();
+            //Using Query Syntax
+            int QSLergestNumber = (from num in intNumbers
+                                   select num).Max();
+            Console.WriteLine("Largest Number = " + MSLergestNumber);
+            Console.ReadKey();
+        }
+    }
+}
