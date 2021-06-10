710187f145c7d778c73f6917a706e6a3582351ac
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 10:24:50 +0530

    Add files via upload

---
+++ LINQ Aggregate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Aggregate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] skills = { "C#.NET", "MVC", "WCF", "SQL", "LINQ", "ASP.NET" };
+            string result = string.Empty;
+            foreach (string skill in skills)
+            {
+                result = result + skill + ", ";
+            }
+            //Find the index position of last comma
+            int lastIndex = result.LastIndexOf(",");
+            //Remove the last comma
+            result = result.Remove(lastIndex);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
