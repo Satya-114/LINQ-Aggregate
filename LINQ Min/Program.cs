747220d5a1b458fd24c75343d1e4db946d766884
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-10 10:14:43 +0530

    Add files via upload

---
+++ LINQ Min/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ_Min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intNumbers = new int[] { 60, 80, 50, 90, 10, 30, 70, 40, 20, 100 };
+            //Using Method Syntax
+            int MSLowestNumber = intNumbers.Min();
+            //Using Query Syntax
+            int QSLowestNumber = (from num in intNumbers
+                                  select num).Min();
+            Console.WriteLine("Lowest Number = " + MSLowestNumber);
+            Console.ReadKey();
+        }
+    }
+}
